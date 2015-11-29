7ade6781a69e441117aa06f0477827ca7fa2b81b
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2015-11-29 13:25:06 +0200

    Allow splitting dataset

--- src/DataSet/DataSet.scala
+++ src/DataSet/DataSet.scala
@@ -1,9 +1,20 @@
 package DataSet
 
+import breeze.numerics._
+
+import scala.util.Random
+
 class DataSet(val input: List[Sample]) {
     val inputLayerSize: Int = input.head.input.activeSize
     val outputLayerSize: Int = input.head.target.activeSize
 
+    def split(): (DataSet, DataSet) = {
+        val all = Random.shuffle(input)
+        val training = all.slice(0, round(input.length * 0.9).toInt)
+        val validation = all.drop(training.length)
+
+        (new DataSet(training), new DataSet(validation))
+    }
 
     override def toString = input.mkString("DataSet(\n\t", "\n\t", "\n)")
 }
